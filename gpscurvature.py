8105021d98c8503fc347b4cd18e21aa38218b3fd
davidhopes <davehopes@gmail.com>
2018-11-10 12:22:14 +0100

    Added get_intial_bearing function to gpscurvature.py

--- gpscurvature.py
+++ gpscurvature.py
@@ -5,7 +5,7 @@ __author__ = "David George Hopes"
 def get_distance(latitude1, longitude1, latitude2, longitude2):
     """
     Uses the Haversine formula with the mean Earth radius, accurate enough for
-    smaller distances
+    smaller distances.
     
     @param latitude1: the latitude of point 1, in degrees
     @param longitude1: the latitude of point 1, in degrees
@@ -18,28 +18,46 @@ def get_distance(latitude1, longitude1, latitude2, longitude2):
     
     # latitude and longitude measured in degrees, convert to radians
     phi1 = math.radians(latitude1)
-    print("phi1: {}".format(phi1))
-
     phi2 = math.radians(latitude2) 
-    print("phi2: {}".format(phi2))
     
-    #
-    delta_phi_degrees = latitude2 - latitude1
-    print("delta_phi_degrees: {}".format(delta_phi_degrees))
-    delta_phi = math.radians(delta_phi_degrees)
-    print("delta_phi: {}".format(delta_phi))
-    delta_lambda_degrees = longitude2 - longitude1
-    print("delta_lambda_degrees: {}".format(delta_lambda_degrees))
-    delta_lambda = math.radians(delta_lambda_degrees)
-    print("delta_lambda: {}".format(delta_lambda))
+    # calculate deltas in degrees, convert to radians
+    delta_phi = math.radians(latitude2 - latitude1)
+    delta_lambda = math.radians(longitude2 - longitude1)
     
     a = math.sin(delta_phi/2.0)**2.0 + math.cos(phi1)*math.cos(phi2)*math.sin(delta_lambda/2.0)**2.0
-    print("a: {}".format(a))
     
     c = 2.0*math.atan2(math.sqrt(a), math.sqrt(1.0 - a))
-    print("c: {}".format(c))
-
 
     distance = radius * c
     
     return distance
+
+def get_bearing(latitude1, longitude1, latitude2, longitude2):
+
+    phi1 = math.radians(latitude1)
+    phi2 = math.radians(latitude2) 
+
+    delta_lambda = math.radians(longitude2 - longitude1)
+
+    y = math.sin(delta_lambda/2.0) * math.sin(phi2)
+    x = math.cos(phi1) * math.sin(phi2) - math.sin(phi1) * math.cos(phi2) *math.cos(delta_lambda)
+
+    bearing_radians = math.atan2(y, x)
+    bearing_degrees = math.degrees(bearing_radians)
+
+    return bearing_degrees
+
+def get_initial_bearing(start_latitude, start_longitude, end_latitude, end_longitude):
+
+    phi1 = math.radians(start_latitude)
+    phi2 = math.radians(end_latitude) 
+
+    delta_lambda = math.radians(end_longitude - start_longitude)
+
+    y = math.sin(delta_lambda/2.0) * math.sin(phi2)
+    x = math.cos(phi1) * math.sin(phi2) - math.sin(phi1) * math.cos(phi2) *math.cos(delta_lambda)
+
+    bearing_radians = math.atan2(y, x)
+    bearing_degrees = math.degrees(bearing_radians)
+
+    return bearing_degrees
